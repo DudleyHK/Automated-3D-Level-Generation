8c4f29085bcebc344f386f7f61493b431b6feced
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2017-12-20 00:16:29 +0000

    added ImportData scripts. Next.. workout how to change data in csv file

---
+++ Assets/Scripts/CSVFile/CSVManager.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSVManager : MonoBehaviour 
+{
+    private static string filePath = Application.dataPath + "/CSV/Saved_Probabilities.csv";
+    
+
+    public static void SaveFile(string type, float value)
+    {
+        
+        StreamWriter writer = new StreamWriter(filePath);
+        writer.WriteLine("Water, Grass, Mud");
+        
+    }
+}
---
+++ Assets/Scripts/CSVFile/ImportData/ImportedDataContainer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ImportedDataContainer 
+{
+	public string ID; 
+	private List<ImportedDataPoint> data = new List<ImportedDataPoint>();
+
+	public void AddDataPoint(ImportedDataPoint dataPoint)
+    {
+		data.Add(dataPoint);
+	}
+	
+	public ImportedDataPoint GetData(string id)
+    {
+		for(int i = 0; i < data.Count; i++)
+        {
+			if(data[i].key == id) return data[i];
+		}
+		Debug.LogWarning("Could not find Value for Key " + id);
+		return null;
+	}
+
+
+    public void SetData(string id, string value)
+    {
+        for(int i = 0; i < data.Count; i++)
+        {
+            if(data[i].key == id)
+            {
+                Debug.Log("setting key id " + id + " which has a current value of " + data[i].value + ", to " + value);
+                data[i].value = value;
+            }
+        }
+    }
+}
