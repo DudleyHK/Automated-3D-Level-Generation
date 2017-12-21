751dd1ec5f42f3ad520380cc92e5ffb91813049b
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2017-12-21 14:41:46 +0000

    parse level and read out the number of times a direction is taken, and what the tile will be. Next... work input Air into grid.

---
+++ Assets/Scripts/Parse Level/ParseLevel.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParseLevel : MonoBehaviour 
+{
+    private enum Directions
+    {
+        Right, 
+        Up, 
+        Forward
+    }
+
+
+    [SerializeField]
+    private GameObject level;
+    [SerializeField]
+    private List<GameObject> tiles;
+    [SerializeField]
+    private List<Directions> directions = new List<Directions>(new Directions[]
+    {
+        Directions.Right,
+        Directions.Up,
+        Directions.Forward
+    });
+    [SerializeField]
+    private List<ArrayList> outputData = new List<ArrayList>();
+    [SerializeField]
+    private Vector3 startPosition = new Vector3(100f, 100f, 100f);
+    [SerializeField]
+    private int startIdx = -1;
+    [SerializeField]
+    private bool parseLevel = false;
+    [SerializeField]
+    private Material complete;
+
+    private CSVManager csvManager;
+
+
+
+    private void Update()
+    {
+        if(parseLevel)
+        {
+            Init();
+            Run();
+            OutputResult();
+            parseLevel = false;
+        }
+    }
+
+    /// <summary>
+    /// Add the chlid tile to a list and check if its positiion is lower in all directions than the current lowest.
+    ///     This is to find out where the min position is.
+    /// </summary>
+    public void Init()
+    {
+        csvManager = FindObjectOfType<CSVManager>();
+        if(!csvManager) Debug.Log("ERROR: CSVManager object is not active in Scene");
+
+        if(!level) Debug.Log("ERROR: Level object has not been set.");
+        int idx = 0;
+        foreach(Transform child in level.transform)
+        {
+            tiles.Add(child.gameObject);
+
+            if(child.transform.position.x < startPosition.x && 
+               child.transform.position.y < startPosition.y &&
+               child.transform.position.z < startPosition.z)
+            {
+                startPosition = child.transform.position;
+                startIdx = idx;
+            }
+            idx++;
+        }
+    }
+
+    /// <summary>
+    /// Currently ignoring startIdx
+    /// </summary>
+    public void Run()
+    {
+        foreach(var tile in tiles)
+        {
+            tile.GetComponent<MeshRenderer>().material = complete;
+
+            foreach(var direction in directions)
+            {
+                var tag = tile.tag + " " + direction.ToString();
+
+                var dir = GetDirectionVector(direction);
+                var neighbourTag = GetTagOfNeighbour(tile.transform.position, dir);
+                if(neighbourTag == "")
+                {
+                    // Debug.Log("Neighbour in direction " + direction.ToString() + " is nothingness");
+                    continue;
+                }
+
+
+                ArrayList list;
+                if(AlreadyCreated(tag, neighbourTag, out list))
+                {
+                    var value = (float)list[2];
+                    value++;
+
+                    list[2] = value;
+                    Debug.Log("Updated Cell From " + list[0] + " to " + list[1] + " with " + list[2]);
+
+                }
+                else
+                {
+                    ArrayList newList = new ArrayList(3);
+                    newList.Add(tag);
+                    newList.Add(neighbourTag);
+                    newList.Add(1f);
+
+                    outputData.Add(newList);
+                    //  outputData.Add(new ArrayList { tag, neighbourTag, 1});
+                    Debug.Log("From " + newList[0] + " to " + newList[1] + " are being added to outputData with data of " + newList[2]);
+                }
+
+            }
+            
+        }
+    }
+
+
+    private bool AlreadyCreated(string from, string to, out ArrayList outList)
+    {
+        outList = null;
+
+        foreach(var list in outputData)
+        {
+            outList = list;
+
+            if(list[0] as string == from &&
+                list[1] as string == to)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public void OutputResult()
+    {
+        csvManager.SetProbabilityValues(outputData);
+    }
+
+
+
+    private Vector3 GetDirectionVector(Directions dir)
+    {
+        Vector3 direction = Vector3.zero;
+        switch(dir)
+        {
+            case Directions.Right:   direction = Vector3.right;   break;
+            case Directions.Up:      direction = Vector3.up;      break;
+            case Directions.Forward: direction = Vector3.forward; break;
+        }
+        return direction;
+    }
+
+    private string GetTagOfNeighbour(Vector3 origin, Vector3 dir)
+    {
+        RaycastHit hitInfo;
+        var hit = Physics.Raycast(origin, dir, out hitInfo);
+        if(hit)
+        {
+            return hitInfo.collider.tag;
+        }
+        else
+        {
+         //   Debug.Log("Raycast at position " + origin + " didn't hit anything");
+        }
+
+        return "";
+    }
+}
---
+++ Assets/Scripts/Probabilities Management/CSVManager.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mono.Csv;
+
+
+public class CSVManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<float> probabilities = new List<float>();
+    [SerializeField]
+    private List<string> rowIds   = new List<string>();
+    [SerializeField]
+    private List<string> columnIds = new List<string>();
+
+    private List<List<string>> gridData = new List<List<string>>();
+    private string csvPath = "..//CTPPrototype//Assets//OutputData//";
+    private string csvFile = "Test.csv";
+
+
+
+    private void Start()
+    {
+        Read();
+    }
+
+    private void Read()
+    {
+        gridData = CsvFileReader.ReadAll(csvPath + csvFile, System.Text.Encoding.GetEncoding("gbk"));
+
+        Debug.Log("REading and grid data size " + gridData.Count);
+        int rowIdx = 0;
+        int cellIdx = 0;
+        foreach(var row in gridData)
+        {
+            foreach(var cell in row)
+            {
+                if(rowIdx == 0)
+                {
+                    columnIds.Add(cell);
+                }
+
+                if(cellIdx == 0)
+                {
+                    rowIds.Add(cell);
+                }
+                
+                float result;
+                bool isNumeric = float.TryParse(cell, out result);
+                if(isNumeric)
+                {
+                    probabilities.Add(result);
+                }
+
+                cellIdx++;
+            }
+            rowIdx++;
+            cellIdx = 0;
+        }
+    }
+
+
+    private void Write()
+    {
+        int rowIdx = 0;
+        int cellIdx = 0;
+        int probabilityIdx = 0;
+
+
+        foreach(var row in gridData)
+        {
+            foreach(var cell in row)
+            {
+                float result;
+                bool isNumeric = float.TryParse(cell, out result);
+                if(isNumeric)
+                {
+                    gridData[rowIdx][cellIdx] = probabilities[probabilityIdx].ToString();
+                    //Debug.Log("Probability idx is " + probabilityIdx + " and gridDataIdx is " + ((cellIdx * row.Count) + rowIdx));
+                    probabilityIdx++;
+                }
+                cellIdx++;
+            }
+            rowIdx++;
+            cellIdx = 0;
+        }
+        CsvFileWriter.WriteAll(gridData, csvPath + csvFile, System.Text.Encoding.GetEncoding("gbk"));
+    }
+
+
+    /// <summary>
+    /// Iterate through the output data and get the row and column index of the passed in data.
+    ///  Use these indexs to find the position in the gridData list, then convert that idx to the 
+    ///  probabilities list idx.
+    /// </summary>
+    /// <param name="outputData"></param>
+    public void SetProbabilityValues(List<ArrayList> outputData)
+    {
+        Debug.Log("SETTING NEW PROBABILITY VALUES");
+        Debug.Log("OutputData size " + outputData.Count);
+
+        foreach(var data in outputData)
+        {
+            var rowName = (string)data[0];
+            var columnName = (string)data[1];
+            var probability = (float)data[2];
+
+            Debug.Log("From " + rowName + " To " + columnName + " new probability is " + probability);
+
+            for(var i = 0; i < rowIds.Count; i++)
+            {
+                for(var j = 0; j < columnIds.Count; j++)
+                {
+                    if(columnIds[j] == columnName &&
+                        rowIds[i] == rowName)
+                    {
+                        Debug.Log("Row " + i + " and Col " + j);
+
+                        // Debug.Log("From " + rowName + " To " + columnName + " new probability is " + probability);
+                        // Debug.Log("Row ID " + j + " and Column ID " + i);
+                        Debug.Log("Columns Count " + columnIds.Count);
+
+                        var gridIdx = (i * columnIds.Count) + j;
+                        var probabilityIdx = ConvertToProbabilitiesIdx(gridIdx);
+
+                        Debug.Log("GridIdx is " + gridIdx + " and probabilitiesIdx is " + probabilityIdx);
+                        probabilities[probabilityIdx] = probability;
+                    }
+                }
+            }
+        }
+    }
+
+    private int ConvertToProbabilitiesIdx(int idx)
+    {
+        int counter = 0;
+        int probabilityIdx = 0;
+
+        foreach(var row in gridData)
+        {
+            foreach(var cell in row)
+            {
+                if(idx == counter)
+                {
+                    break;
+                }
+
+                float result;
+                bool isNumeric = float.TryParse(cell, out result);
+                if(isNumeric)
+                {
+                    probabilityIdx++;
+                }
+                counter++;
+            }
+        }
+        return probabilityIdx;
+    }
+    
+
+    private void OnApplicationQuit()
+    {
+        Write();
+    }
+}
